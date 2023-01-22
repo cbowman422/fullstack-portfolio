d88db6c67481386ebb7fe19f777f20629b2ad8a9
cbowman422 <chrisb0wman@hotmail.com>
2023-01-22 17:54:47 -0500

    about edit

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className="aboutContainer">
       <h1 className="aboutTitle"> About me</h1>
-      <p className="aboutParagraph"> With a passion for art and a degree in mechanical engineering. Some of my hobbies besides programming include painting, mathematics, being a good role model for my dog, creating music, rock climbing, skiing, and martial arts.</p>
+      <p className="aboutParagraph"> With a passion for art and a degree in mechanical engineering. Some of my hobbies besides programming include painting, mathematics, being a good role model for my dog, creating music, rock climbing, and skiing.</p>
       <h1 className="aboutTitle"> Technical Skills</h1>
       <p className="aboutParagraph"> Languages - Typescript | Python | JavaScript | SQL | Mongo | JSON | EJS | HTML | CSS | MATLAB | Arduino <br />
 Databases - PostgreSQL | MongoDB <br />
