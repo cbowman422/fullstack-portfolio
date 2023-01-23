ec0369dea45cfe1cd6a497a5ad8fb345c67869b4
cbowman422 <chrisb0wman@hotmail.com>
2023-01-22 19:56:08 -0500

    contact container

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -25,9 +25,11 @@ function Contact(props) {
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => (
+    <div className="contactMargin">
     <div className="contactContainer">
       <h1 className="contactInfo">{contact.name}</h1>
       <h1 className="contactInfo">{contact.email}</h1>
+    </div>
     </div>
   );
 
