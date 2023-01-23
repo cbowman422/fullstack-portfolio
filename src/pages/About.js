3b16d816264e9b20241fce6fe83781d0d05f92bb
cbowman422 <chrisb0wman@hotmail.com>
2023-01-22 19:03:52 -0500

    container colors added and margins

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,7 @@ import "./pages_css/About.css"
 
 const About = () => {
   return (
+    <div className="aboutMargin">
     <div className="aboutContainer">
       <h1 className="aboutTitle"> About me</h1>
       <p className="aboutParagraph"> With a passion for art and a degree in mechanical engineering. Some of my hobbies besides programming include painting, mathematics, being a good role model for my dog, creating music, rock climbing, and skiing.</p>
@@ -13,6 +14,7 @@ Databases - PostgreSQL | MongoDB <br />
 Libraries and Frameworks - React.js | Express.js | Django | Flask <br />
 Softwares - ABAQUS | Altair HyperWorks | ANSYS | Fluent | SolidWorks | Mathcad | Microsoft Office <br />
 Interpersonal Skills - Communication Skills | Collaboration  </p>
+    </div>
     </div>
   )
 }
