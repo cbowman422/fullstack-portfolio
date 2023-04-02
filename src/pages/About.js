f7c820e080d048458e74943f6218024c25d7be60
cbowman422 <chrisb0wman@hotmail.com>
2023-04-02 10:30:19 -0400

    skill update

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,8 +12,7 @@ const About = () => {
       <h2 className="aboutTitle"> Technical Skills ⚡ </h2>
       <p className="aboutParagraph"> Languages - Typescript | Python | JavaScript | SQL | Mongo | JSON | EJS | HTML | CSS | MATLAB <br />
 Databases - PostgreSQL | MongoDB <br />
-Libraries and Frameworks - React.js | Express.js | Django | Flask <br />
-Softwares - ABAQUS | Altair HyperWorks | ANSYS | Fluent | SolidWorks | Mathcad <br />
+Libraries and Frameworks - React.js | Express.js | Django <br />
 Interpersonal Skills - Communication Skills | Collaboration  </p>
     </div>
     </div>
