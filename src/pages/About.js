31f78c6f03cb2956c1ee9baf96047cfe1b457d04
cbowman422 <chrisb0wman@hotmail.com>
2023-05-09 20:47:44 -0400

    soft skills

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,8 +12,7 @@ const About = () => {
       <h2 className="aboutTitle"> Technical Skills ⚡ </h2>
       <p className="aboutParagraph"> Languages - JavaScript | Python | SQL | Mongo | JSON | EJS | HTML | CSS | MATLAB <br />
 Databases - PostgreSQL | MongoDB <br />
-Libraries and Frameworks - React.js | Express.js | Django <br />
-Interpersonal Skills - Communication Skills | Collaboration  </p>
+Libraries and Frameworks - React.js | Express.js | Django  </p>
     </div>
     </div>
   )
