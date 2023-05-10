fe241dc341ed4b9cf67658ff8ddf216c0838d479
cbowman422 <chrisb0wman@hotmail.com>
2023-05-09 20:07:49 -0400

    about update

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ const About = () => {
     <img className="aboutImage" src="https://imgur.com/x33b0yt.png" alt="selfie"/>
     <div className="aboutContainer">
       <h2 className="aboutTitle"> About Me 🎨 </h2>
-      <p className="aboutParagraph"> Seeking position in full stack development, incorporating my versatile experience in software, mathematics, and engineering. Creative, highly-motivated, and adaptable professional who is able to work with stakeholders to create useful and user-friendly applications. Outside of work, I enjoy painting, creating music, and spending time outdoors with my dog. </p>
+      <p className="aboutParagraph"> Software developer specializing in full stack JavaScript technologies. Obtained a Software Engineering Immersive certificate from General Assembly. Worked on multiple teams to develop full stack applications to provide unique perspectives and experience. Prior to software development I completed my Bachelors degree in Mechanical Engineering from the University of Massachusetts, Amherst and worked as a Structural Engineer for General Dynamics. Looking to leverage my unique experiences and knowledge in software development to ensure the delivery of quality products. </p>
       <h2 className="aboutTitle"> Technical Skills ⚡ </h2>
       <p className="aboutParagraph"> Languages - JavaScript | Python | SQL | Mongo | JSON | EJS | HTML | CSS | MATLAB <br />
 Databases - PostgreSQL | MongoDB <br />
