384dad35a27f2a5cc23483dc2bb9f53e6d67865d
cbowman422 <chrisb0wman@hotmail.com>
2023-05-19 11:10:32 -0400

    about adjustment

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ const About = () => {
       <h2 className="aboutTitle"> About Me 🎨 </h2>
       <p className="aboutParagraph">Software developer specializing in full stack JavaScript technologies. Obtained a certificate from General Assembly’s Software Engineering program. Worked on multiple teams to develop full stack applications. Prior to software development I have experience in mechanical engineering. Looking to leverage my unique background and knowledge in software development to ensure the delivery of quality products.</p>
       <h2 className="aboutTitle"> Technical Skills ⚡ </h2>
-      <p className="aboutParagraph"> Languages - JavaScript | Python | SQL | Mongo | JSON | EJS | HTML | CSS | MATLAB <br />
+      <p className="aboutParagraph"> Languages - JavaScript | Python | SQL | Mongo | JSON | EJS | HTML | CSS <br />
 Libraries and Frameworks - React.js | Express.js | Django | Flask </p>
 Databases - PostgreSQL | MongoDB <br />
     </div>
