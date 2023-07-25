9727c6f60c3b379be1bb59048ec997a59cd85c0b
cbowman422 <chrisb0wman@hotmail.com>
2023-07-25 13:00:17 -0400

    project button padding

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,9 +7,9 @@ const About = () => {
     <div id="aboutMargin">
     <img className="aboutImage" src="https://imgur.com/x33b0yt.png" alt="selfie"/>
     <div className="aboutContainer">
-      <h2 className="aboutTitle"> About Me 🎨 </h2>
+      <h2 className="aboutTitle"> About Me </h2>
       <p className="aboutParagraph">Software developer specializing in full stack JavaScript technologies. Worked on multiple teams to develop full stack applications. Prior to software development I have experience in mechanical engineering. Looking to leverage my unique background and knowledge in software development to ensure the delivery of quality products.</p>
-      <h2 className="aboutTitle"> Technical Skills ⚡ </h2>
+      <h2 className="aboutTitle"> Technical Skills </h2>
       <p className="aboutParagraph"> Languages - JavaScript | Python | SQL | Mongo | JSON | EJS | HTML | CSS <br />
         Libraries and Frameworks - React.js | Express.js | Django | Flask <br />
         Databases - PostgreSQL | MongoDB </p>
