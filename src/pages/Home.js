3ab4196e07d57d479860168a10a3da9aa5cd76dd
cbowman422 <chrisb0wman@hotmail.com>
2023-07-25 12:51:28 -0400

    home list and title animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,13 @@ import "./pages_css/Home.css";
 function Home(props) {
   return < div id="homeContainer" >
   <h1 className="homeTitle">
-  Christopher Bowman <span className="homeTitleSmall" > Full Stack Developer </span>
-  </h1>
+  Christopher Bowman </h1> 
+  <div className="homeTitleSmall">
+      <li className="animation"> Software Developer </li>
+      <li className="animation"> Full Stack Developer </li>
+      <li className="animation"> Mechanical Engineer </li>
+      <li className="animation"> Software Developer </li>
+  </div>
   </ div >
 }
 
