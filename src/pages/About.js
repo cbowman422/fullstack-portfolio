6966a5e96404e63a09e3ef8b717b2490cf5fa832
cbowman422 <chrisb0wman@hotmail.com>
2023-08-11 12:08:02 -0400

    about edit

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ const About = () => {
     <img className="aboutImage" src="https://imgur.com/x33b0yt.png" alt="selfie"/>
     <div className="aboutContainer">
       <h2 className="aboutTitle"> About Me </h2>
-      <p className="aboutParagraph">Software engineer specializing in full stack JavaScript technologies. Worked on multiple teams to develop full stack applications. Prior to software development I have experience in mechanical engineering. Looking to leverage my unique background and knowledge in software development to ensure the delivery of quality products.</p>
+      <p className="aboutParagraph">Motivated software engineer with a strong focus on full stack JavaScript technologies. Contributed to multiple teams, collectively designing and developing full stack applications. Prior to software development I gained valuable experience in mechanical engineering. Committed to continuous learning and staying up-to-date with industry trends. Seeking to leverage my experience in software engineering to drive innovation and contribute to impactful projects.</p>
       <h2 className="aboutTitle"> Technical Skills </h2>
       <p className="aboutParagraph"> Languages - JavaScript | Python | SQL | Mongo | JSON | EJS | HTML | CSS <br />
         Libraries and Frameworks - React.js | Next.js | Express.js | Django | Flask <br />
